9a46e65e0bdeac44ff504d02dc502df2c71bb950
Felipe <felipesaziki@gmail.com>
2026-06-17 18:37:21 -0300

    ksdjfhg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { CidadeBlumenau } from './cidade-blumenau/cidade-blumenau';
 import { FormularioBasico } from './formulario-basico/formulario-basico';
 import { Navbar } from './navbar/navbar';
 import { ClientesCadastrar } from './clientes/clientes-cadastrar/clientes-cadastrar';
+import { BoaVindas } from './boa-vindas/boa-vindas';
 
 export const routes: Routes = [
     { path: "cidade/blumenau", loadComponent: () => CidadeBlumenau},
     { path: "formulario/campo-basico", loadComponent: () => FormularioBasico},
     { path: "navbar/navbar", loadComponent: () => Navbar},
-    { path: "clientes/clientes-cadastrar", loadComponent: () => ClientesCadastrar}
+    { path: "clientes/clientes-cadastrar", loadComponent: () => ClientesCadastrar},
+    { path: "/", loadComponent: () => BoaVindas}
 ];
